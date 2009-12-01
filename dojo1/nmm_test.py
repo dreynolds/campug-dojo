d27cf250808a228f1ab9f5f144b8a6150bec04ea
Joe <joe@joe-laptop>
2009-12-01 20:56:53 +0000

    Robin

--- dojo1/nmm_test.py
+++ dojo1/nmm_test.py
@@ -7,6 +7,7 @@ class NineMensMorrisTest(unittest.TestCase):
 
     def testRepresentation(self):
         Board = nmm.Board("0", "0", "a7,d7,g4", "a1,b2,c3", "W")
+        print Board.getBoard()
         self.assertEquals(Board.getPosition(), "0,0 a7,d7,g4 a1,b2,c3 W")
         self.assertEquals(Board.getBoard(),
 """\
